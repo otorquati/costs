7b30dc49198de28a48d6559f4aacace990da8755
Osvaldo <torquati.osvaldo@gmail.com>
2023-06-09 01:47:04 -0300

    Lesson #18
    Structuring the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import {BrowserRouter as Router, Routes } from 'react-router-dom';
+// Importação dos Componentes
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <link to="/">Home</link>
+        <link to="/Company">Empresa</link>
+        <link to="/Contact">Contatos</link>
+        <link to="/NewProject">Novo Projeto</link>
+      </div>
+      <Routes>
+        <route exact path="/" >
+          <Home/>
+        </route> 
+        <route exact path="/company" >
+          <Company/>
+        </route> 
+        <route exact path="/contact" >
+          <Contact/>
+        </route> 
+        <route exact path="/newproject" >
+          <NewProject/>
+        </route> 
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Container.js
@@ -0,0 +1,8 @@
+import styles from '/Container.module.css'
+
+function Container(props) {
+  return(<div className={`${styles.container} ${styles[props.customClass]}`}></div>
+  )
+}
+
+export default Container
