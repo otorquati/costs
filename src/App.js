38344a0ed05776dd5de8242be08d25b58ace7159
Osvaldo <torquati.osvaldo@gmail.com>
2023-06-09 02:17:36 -0300

    Lesson #18 - Updating

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import {BrowserRouter as Router, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Importação dos Componentes
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import NewProject from './components/pages/NewProject'
-
+import Container from './components/layout/Container'
 function App() {
   return (
     <Router>
-      <div>
-        <link to="/">Home</link>
-        <link to="/Company">Empresa</link>
-        <link to="/Contact">Contatos</link>
-        <link to="/NewProject">Novo Projeto</link>
-      </div>
       <Routes>
-        <route exact path="/" >
-          <Home/>
-        </route> 
-        <route exact path="/company" >
-          <Company/>
-        </route> 
-        <route exact path="/contact" >
-          <Contact/>
-        </route> 
-        <route exact path="/newproject" >
-          <NewProject/>
-        </route> 
-      </Routes>
+      <Container customClass="min-height">
+        <Route exact path="/" element={<Home/>}/>;
+        <Route path="/company" element={<Company/>}/>;
+        <Route path="/contact" element={<Contact/>}/>;
+        <Route path="/newproject" element={<NewProject/>}/>; 
+      </Container>
       <p>Footer</p>
+      </Routes>
     </Router>
   );
 }
