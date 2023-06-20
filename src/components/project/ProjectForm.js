993264b92f92ac1b3b7fadd63b0e279e327608ab
Osvaldo <torquati.osvaldo@gmail.com>
2023-06-20 00:59:38 -0300

    Lesson #23 & #24
    Conectando a API e adicionando dados

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -6,8 +6,9 @@ import Submit from '../form/Submit'
 //Importação dos Estilos
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}){
+function ProjectForm({ handleSubmit, btnText, projectData }){
   const [categories, setCategories] = useState([])
+  const [project, setProject] = useState(projectData || {})
   useEffect(() => {
     fetch('http://localhost:5000/categories', {
     method: 'GET',
@@ -22,24 +23,48 @@ function ProjectForm({btnText}){
   .catch((err) => console.log(err))
   }, [])
 
+  const submit =(e) => {
+    e.preventDefault()
+    handleSubmit(project)
+  }
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]:e.target.value })
+    //console.log(project)
+  }
+  function handleCategory(e) {
+    setProject({ ...project, 
+      category: {
+        id:e.target.value,
+        name: e.target.options[e.target.selectedIndex].text, 
+      },
+      })
+  }
+
   return (
-    <form className={styles.form} action="">
+    <form onSubmit={submit} className={styles.form} action="">
       <Input 
           type="text"
           text="Nome do projeto"
           name="name"
           placeholder="Insira o nome do projeto"
+          handleOnChange={handleChange}
+          value={project.name ? project.name : ''}
         />
       <Input 
           type="number"
           text="Orçamento do projeto"
           name="budget"
           placeholder="Insira o orçamento total"
+          handleOnChange={handleChange}
+          value={project.budget ? project.budget : ''}
         />
         <Select 
           name="category_id" 
           text="Selecione a categoria" 
           options={categories} 
+          handleOnChange={handleCategory}
+          value={project.category ? project.category.id : ''}
         />
         <Submit text={btnText} />
     </form>
